e014ca133a3e5dbb37829b8344d5fb89e1a9c678
venkateshsekaran <venkateshsekaran18@gmail.com>
2022-06-06 22:21:15 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  let [number, setnumber] = useState("");
+  let [check, setcheck] = useState("");
+  let checkHandler = (event) => {
+    setnumber(event.target.value);
+    if (event.target.value % 2 === 0) {
+      console.log("Even");
+      setcheck(`${event.target.value} is Even`);
+    } else {
+      console.log("Odd");
+      setcheck(`${event.target.value} is Odd`);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6">
+          <pre>Enter No is:{JSON.stringify(number)}</pre>
+          <pre>{JSON.stringify(check)}</pre>
+          <form>
+            <label>Enter number to check odd or even: </label>
+            <input type="number" onMouseLeave={checkHandler} />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
